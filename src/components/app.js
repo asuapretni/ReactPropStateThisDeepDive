2c3ad623767600819f65e172f050437dea312bf0
MacbookPro <macbookpro@MBPdeMacbookPro.lan>
2023-07-29 10:32:02 +0200

    State manipulation

--- src/components/app.js
+++ src/components/app.js
@@ -8,7 +8,6 @@ export default class App extends Component {
         <h1>React, Props and State Deep Dive</h1>
         <h2>React Skeleton</h2>
         <JournalList heading="List 1"/>
-        <JournalList heading="List 2"/>
       </div>
     );
   }
--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -22,11 +22,42 @@ export default class JournalList extends Component {
         };
     }
 
+    clearEntries = () => {
+        this.setState({ journalData: [], isOpen: false });
+    };
+
+    showAllEntries = () => {
+        this.setState({ journalData: rawJournalData, isOpen: true });
+    };
+
+    toggleStatus = () => {
+        if (this.state.isOpen) {
+            this.setState({ journalData: [], isOpen: false })
+        } else {
+            this.setState({ journalData: rawJournalData, isOpen: true });
+        }
+    }
+
     render() {
+        const journalEntries = this.state.journalData.map(journalEntry => {
+            return (
+                <div key={journalEntry.title}>
+                    <JournalEntry 
+                    title={journalEntry.title} 
+                    content={journalEntry.content} 
+                    />
+                </div>
+            );
+        });
+
     return (
       <div>
       <h2>{this.props.heading}</h2>
-      <JournalEntry title="Some title" content="More content" />
+      {journalEntries}
+
+      <button onClick={this.clearEntries}>Clear Journal Entries</button>
+      <button onClick={this.showAllEntries}>Show All Entries</button>
+      <button onClick={this.toggleStatus}>Toggle Entries</button>
       </div>
     );
     }
